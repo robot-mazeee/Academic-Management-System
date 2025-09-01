f5d047412ce4eff9329d00a19da306664ecc70a6
robot-mazeee <madalenacmo@gmail.com>
2025-09-01 09:40:22 +0100

    make curricular unit actions only visible to administrator

--- src/frontend/src/views/curricular-units/CurricularUnitsView.vue
+++ src/frontend/src/views/curricular-units/CurricularUnitsView.vue
@@ -27,7 +27,7 @@
     class="text-left"
     no-data-text="Sem unidades curriculares a apresentar."
   >
-    <template v-slot:[`item.actions`]="{ item }">
+    <template v-if="roleStore.isAdministrator" v-slot:[`item.actions`]="{ item }">
 			<div class="d-flex align-center justify-center ga-2">
 				<EditCurricularUnitDialog :curricular-unit-to-edit="item" @curricular-unit-edited="getCurricularUnits" />
 				<v-icon @click="deleteCurricularUnit(item)" color="red" class="cursor-pointer">
