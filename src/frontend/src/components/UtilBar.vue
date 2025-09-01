3d11ec96f496246617acf064f2fc7b5f93d5f919
robot-mazeee <madalenacmo@gmail.com>
2025-09-01 01:06:41 +0100

    change role dropdown

--- src/frontend/src/components/UtilBar.vue
+++ src/frontend/src/components/UtilBar.vue
@@ -1,57 +1,38 @@
 <template>
-  <v-app-bar color="secondary" :height="36" class="px-2">
-    <v-toolbar-items>
-      <v-btn
-        href="https://dei.tecnico.ulisboa.pt/"
-        selected-class="no-active"
-        class="dei-title"
-        size="small"
-      >
-        Departamento de Engenharia Informática
-      </v-btn>
-    </v-toolbar-items>
-    <v-spacer />
-    <span>Current Role: {{ currentRole }}</span>
-    <v-spacer />
-    <v-toolbar-items class="align-center">
-      <DarkModeSwitch />
-    </v-toolbar-items>
-
-    <v-toolbar-items class="ms-2">
-      <v-btn size="small" @click="changeRole('student')">Aluno</v-btn>
-      <v-btn size="small" @click="changeRole('teaching_assistant')">Professor Assistente</v-btn>
-      <v-btn size="small" @click="changeRole('main_teacher')">Professor Regente</v-btn>
-      <v-btn size="small" @click="changeRole('administrator')">Administrador</v-btn>
-    </v-toolbar-items>
-    <v-toolbar-items class="ms-2">
-      <v-btn size="small" variant="text">
-        Terminar sessão
-        <v-icon size="small" class="ms-1" icon="mdi-logout"></v-icon>
-      </v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
+	<v-app-bar color="secondary" :height="36" class="px-2">
+		<v-container fluid class="d-flex align-center justify-space-between">
+      <div class="d-flex align-center">
+        <v-btn
+          href="https://dei.tecnico.ulisboa.pt/"
+          selected-class="no-active"
+          class="dei-title"
+          size="small"
+        >
+          DEI
+        </v-btn>
+        <v-spacer />
+				<v-toolbar-items class="align-center">
+					<DarkModeSwitch />
+				</v-toolbar-items>
+			</div>
+		  <v-spacer />
+			<div class="d-flex align-center justify-center" style="flex: 1">
+				<ChangeRoleDropdown />
+			</div>
+		  <v-spacer />
+      <v-toolbar-items class="ms-2">
+        <v-btn size="small" variant="text">
+          Terminar sessão
+          <v-icon size="small" class="ms-1" icon="mdi-logout"></v-icon>
+        </v-btn>
+      </v-toolbar-items>
+		</v-container>
+	</v-app-bar>
 </template>
 
 <script setup lang="ts">
 import DarkModeSwitch from './DarkModeSwitch.vue'
-import { useRoleStore } from '@/stores/role'
-import { ref } from 'vue'
-import { watch } from 'vue'
-
-const roleStore = useRoleStore()
-
-const changeRole = (role: string) => {
-  roleStore.currentRole = role
-}
-
-const currentRole = ref(roleStore.currentRole)
-
-watch(
-  () => roleStore.currentRole,
-  (newRole) => {
-    currentRole.value = newRole
-  }
-)
+import ChangeRoleDropdown from './ChangeRoleDropdown.vue'
 </script>
 
 <style scoped>
