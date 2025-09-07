d00e0a9c572554f544d85fa6ee0c8138abd05575
robot-mazeee <madalenacmo@gmail.com>
2025-09-07 18:24:15 +0100

    implement confirm action dialog

---
+++ src/frontend/src/components/buttons/DeleteButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-icon @click="openDialog" color="red" class="cursor-pointer">
+    mdi-delete
+  </v-icon>
+
+  <ConfirmActionDialog v-model="showDialog" @confirm="doAction" />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import ConfirmActionDialog from '../../views/dialogs/ConfirmActionDialog.vue'
+
+const props = defineProps<{
+  fn: (arg: any) => void
+  item: any
+}>()
+
+const showDialog = ref(false)
+
+function openDialog() {
+  showDialog.value = true
+}
+
+function doAction() {
+  props.fn(props.item)
+}
+</script>
---
+++ src/frontend/src/views/dialogs/ConfirmActionDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-dialog v-model="model" max-width="400">
+    <v-card>
+      <v-card-title>Confirm Action</v-card-title>
+      <v-card-text>
+        De certeza que quer realizar esta ação? Poderá perder informação crítica.
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn text @click="cancel">Cancelar</v-btn>
+        <v-btn color="red" text @click="confirm">Confirmar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+const model = defineModel<boolean>({ required: true })
+
+const emit = defineEmits<{
+  (e: 'confirm'): void
+}>()
+
+function cancel() {
+  model.value = false
+}
+
+function confirm() {
+  emit('confirm')
+  model.value = false
+}
+</script>
